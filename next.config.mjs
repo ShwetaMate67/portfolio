b69725150fb9830494e51036fd225f63bbc989b0
ShwetaMate67 <shwetamate0109@gmail.com>
2025-05-14 19:06:03 -0500

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig = {
+  output: 'export',
+  basePath: isGithubActions ? '/portfolio' : '',
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
   },
 };
 
